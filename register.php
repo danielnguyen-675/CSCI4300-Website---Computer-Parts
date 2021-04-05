5870db94b72519c0f65377b5f9dd3d6a64afa236
Daniel Nguyen <ddn53340@uga.edu>
2021-04-05 18:58:51 -0400

    added php code to connect to database, then the html form inputs were saved into register.php. no querying yet

--- register.php
+++ register.php
@@ -1,3 +1,44 @@
-<?php
+<?php 
+
+    $dsn = 'mysql:host=localhost; dbname=pcparts';
+    $username='root';
     
+    $password='';
+
+    try {
+        $db = new PDO($dsn, $username, $password);
+        echo 'Connected to database successfully';
+    } catch (PDOException $e)
+    {
+        $error=$e->getMessage();
+        echo '<p> Unable to connect to database: ' .$error;
+        exit();
+    }
+
+    $firstName=$_POST['fName'];
+    $lastName=$_POST['lName'];
+    $email=$_POST['email'];
+    $password=$_POST['password'];
+    $phoneNumber=$_POST['phone'];
+    //PHP code + SQL code to QUERY into the DATABASE
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>Registration Authentication</title>
+</head>
+<body>
+    <main>
+        <p>First name is: <?php echo $firstName; ?></p>
+        <p>Last name is: <?php echo $lastName; ?></p>
+        <p>Email is: <?php echo $email; ?></p>
+        <p>Password is: <?php echo $password; ?></p>
+        <p>Phone number is: <?php echo $phoneNumber; ?></p>
+</main>
+    
+</body>
+</html>
+
