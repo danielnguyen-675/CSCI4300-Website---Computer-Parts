3cd9fdd60672758d18e60c0411967c71a3466d8c
Daniel Nguyen <ddn53340@uga.edu>
2021-04-04 22:32:10 -0400

    test...

--- register.js
+++ register.js
@@ -1,8 +1,8 @@
-var $ = function(id) {
-	return document.getElementById(id);
+var $ = function (id) {
+    return document.getElementById(id);
 }
 
-var checkFields = function() {
+var checkFields = function () {
     //parse fields
     var fName = $("fName").value;
     var lName = $("lName").value;
@@ -25,34 +25,34 @@ var checkFields = function() {
     //check to see if fields are correctly filled
     //check first name
     var regexName = /[a-z ,.'-]+/i;
-    if ( !regexName.test(fName) ) {
+    if (!regexName.test(fName)) {
         req1 = "First name is invalid."
         isValid = false;
     }
 
     //check last name
-    if ( !regexName.test(lName) ) {
+    if (!regexName.test(lName)) {
         req2 = "Last name is invalid."
         isValid = false;
     }
 
     //check email
-    if ( email == "" ) {
+    if (email == "") {
         req3 = "Email address cannot be empty."
         isValid = false;
     }
     else {
         //using email regex from https://www.regular-expressions.info/email.html
         var regexEmail = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}/i;
-        if ( !regexEmail.test(email) ) {
+        if (!regexEmail.test(email)) {
             req3 = "Please enter a valid email address.";
             isValid = false;
         }
     }
 
     //see if email is reentered correctly
-    if ( email.length != 0 ) {
-        if ( email != email2 ) {
+    if (email.length != 0) {
+        if (email != email2) {
             req3b = "Email addresses much match.";
             isValid = false;
         }
@@ -64,54 +64,54 @@ var checkFields = function() {
     var hasNumber = false;
     //special characters
     var special = "!#$%&'()*+,-./<=>?@^_|~";
-    if ( password.length < 8 ) {
+    if (password.length < 8) {
         req4 = "Password must be at least 8 characters long.";
         isValid = false;
     }
     else {
         //check if contains uppercase/unique character
-        for ( var i = 0; i < password.length; i++ ) {
-            if ( password.charAt(i) == password.charAt(i).toUpperCase() ) {
+        for (var i = 0; i < password.length; i++) {
+            if (password.charAt(i) == password.charAt(i).toUpperCase()) {
                 hasUpper = true;
             }
-            if ( !isNaN(password.charAt(i)) ) {
+            if (!isNaN(password.charAt(i))) {
                 hasNumber = true;
             }
-            for ( var j = 0; j < special.length; j++ ) {
-                if ( password.charAt(i) == special.charAt(j) ) {
+            for (var j = 0; j < special.length; j++) {
+                if (password.charAt(i) == special.charAt(j)) {
                     hasUnique = true;
                 }
             }
         }
 
-        if ( !(hasNumber && hasUnique && hasUpper) ) {
+        if (!(hasNumber && hasUnique && hasUpper)) {
             req4 = "Password must contain at least one uppercase letter, a number, and one of the following unique characters:\n! # $ % & ' ( ) * + , - . / < = > ? @ ^ _ | ~";
             isValid = false;
         }
     }
 
     //check to see if password matches
-    if ( password.length != 0 ) {
-        if ( password != password2 ) {
+    if (password.length != 0) {
+        if (password != password2) {
             req4b = "Passwords much match.";
             isValid = false;
         }
     }
 
     //check phone number
-    if ( phone.length = 0 ) {
+    if (phone.length = 0) {
         req5 = "Phone number cannot be empty."
         isValid = false;
     }
     else {
         var regexPhone = /\(\d{3}\) \d{3}-\d{4}/;
-        if ( !regexPhone.test(phone) ) {
+        if (!regexPhone.test(phone)) {
             req5 = "Phone number must be of the form (123) 456-7890";
             isValid = false;
         }
     }
 
-    if ( isValid ) {
+    if (isValid) {
         req1 = "";
         req2 = "";
         req3 = "";
@@ -133,9 +133,9 @@ var checkFields = function() {
     return isValid;
 }
 
-var clearFields = function() {
-	$("req1").innerHTML = "*";
-	$("req2").innerHTML = "*";
+var clearFields = function () {
+    $("req1").innerHTML = "*";
+    $("req2").innerHTML = "*";
     $("req3").innerHTML = "*";
     $("req3b").innerHTML = "*";
     $("req4").innerHTML = "*";
@@ -143,7 +143,7 @@ var clearFields = function() {
     $("req5").innerHTML = "*";
 }
 
-window.onload = function() {
-	$("submit").onclick = checkFields;
-	$("reset").onclick = clearFields;
+window.onload = function () {
+    $("submit").onclick = checkFields;
+    $("reset").onclick = clearFields;
 }
--- register.php
+++ register.php
@@ -1,3 +1,3 @@
 <?php
-    //insert cool PHP here
+    
 ?>
