dd88633c6d3dcd84295dccbfaf2d786861c2e458
Daniel Nguyen <63984469+danielnguyen-675@users.noreply.github.com>
2021-04-16 13:07:59 -0400

    Update displayCategories.php

--- displayCategories.php
+++ displayCategories.php
@@ -14,7 +14,7 @@ $products = $db->query($query);
 <head>
     <meta charset="UTF-8">
     <title>Neweregg</title>
-    <link rel="stylesheet" href="stylesheets/display.css">
+    <link rel="stylesheet" href="stylesheets/homepage.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 </head>
 
